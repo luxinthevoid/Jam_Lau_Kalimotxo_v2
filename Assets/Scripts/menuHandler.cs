fe206f252d9a11da6924017d6dd12a890855a955
Lireloo <166669063+luxinthevoid@users.noreply.github.com>
2026-02-08 13:14:59 +0100

    menu inicio

--- Assets/Scripts/menuHandler.cs
+++ Assets/Scripts/menuHandler.cs
@@ -1,17 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class menuHandler : MonoBehaviour
 {
-    public void clickJugar(int escena)
+    [SerializeField] Button btnJugar;
+    [SerializeField] Button btnOpciones;
+    [SerializeField] Button btnSalir;
+    [SerializeField] Button btnVolver;
+    [SerializeField] TextMeshProUGUI instrucciones;
+
+    void OnEnable()
     {
-        SceneManager.LoadScene(escena);
+        btnJugar.gameObject.SetActive(true);
+        btnOpciones.gameObject.SetActive(true);
+        btnSalir.gameObject.SetActive(true);
+        btnVolver.gameObject.SetActive(false);
+        instrucciones.gameObject.SetActive(false);
+    }
+
+    public void clickJugar()
+    {
+        SceneManager.LoadScene("Cocina");
         Debug.Log("Cargar escena de juego");
     }
 
     public void clickOpciones()
     {
         Debug.Log("Mostrar opciones");
+        btnJugar.gameObject.SetActive(false);
+        btnOpciones.gameObject.SetActive(false);
+        btnSalir.gameObject.SetActive(false);
+        btnVolver.gameObject.SetActive(true);
+        instrucciones.gameObject.SetActive(true);
     }
 
     public void clickSalir()
@@ -23,5 +45,10 @@ public class menuHandler : MonoBehaviour
     public void clickVolver()
     {
         Debug.Log("Mostrar opciones");
+        btnJugar.gameObject.SetActive(true);
+        btnOpciones.gameObject.SetActive(true);
+        btnSalir.gameObject.SetActive(true);
+        btnVolver.gameObject.SetActive(false);
+        instrucciones.gameObject.SetActive(false);
     }
 }
